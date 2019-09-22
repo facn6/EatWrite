74b6989b50993aeaf897959df303979c220d836e
ayah <ayah_kh5@hotmail.com>
2019-09-22 12:52:21 +0300

    add a sign-up route

--- client/src/App/Components/Home.js
+++ client/src/App/Components/Home.js
@@ -15,7 +15,7 @@ const Home = () => {
       <Link to="./list">
         <Button className="brown"> sign-in </Button>
       </Link>
-      <Link to="./list">
+      <Link to="./sign-up">
         <Button className="brown"> sign-up </Button>
       </Link>
     </div>
